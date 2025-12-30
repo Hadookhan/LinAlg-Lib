570d38e1271181cfc4e8b985db8f23f5d7fee051
Hadookhan <Hadikhan05@hotmail.co.uk>
2025-12-30 19:25:43 +0000

    Main and data file to organise system

---
+++ main.py
@@ -0,0 +1,10 @@
+from preset_data import *
+from components import Linalg
+
+def main():
+
+    a_b = Linalg.dot(a,b)
+    print(a_b)
+
+if __name__ == '__main__':
+    main()
--- preset_data.py
+++ preset_data.py
@@ -1,17 +1,16 @@
 import numpy as np
 
-a = np.array([[1,2,3],
-             [4,3,2],
-             [3,4,1]
-             ], dtype='int64')
+a = np.array([
+    [1, 2],
+    [3,4]
+], dtype='int32')
+
 b = np.array([
-    [3,1,2],
-    [1,2,5],
-    [0,1,3]
-], dtype='int64')
+    [5],
+    [6]
+], dtype='int32')
 
 c = np.array([
     [1],
-    [3],
-    [0]
-])
+    [3]
+], dtype='int32')
